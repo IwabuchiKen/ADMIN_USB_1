d207946ea26ad8009c3bbda31d46ea0a97a64609
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-03-02 08:22:07 +0900

    add: README; modify: setpath~.bat

--- setpath_win_xp.bat
+++ setpath_win_xp.bat
@@ -33,11 +33,16 @@ set JAVA_HOME=C:\WORKS\PROGRAMS\Java
 set JAVA_HOME_BIN=C:\WORKS\PROGRAMS\Java\bin
 set PYTHON_HOME=C:\WORKS\PROGRAMS\Python266
 set NBP_HOME=C:\WORKS\WORKSPACES\G_nbp_new
+set ANT_HOME=C:\WORKS\PROGRAMS\apache-ant-1.8.3
+set ANT_HOME_BIN=%ANT_HOME%\bin
+set ADMIN_HOME=C:\WORKS\WORKSPACES\ADMIN_USB_1
 
 set path=%path%;C:\WORKS\PROGRAMS\Git\bin;C:\WORKS\PROGRAMS\MinGW\bin;
 set path=%path%;%JAVA_HOME_BIN%
 set path=%path%;%PYTHON_HOME%;
 set path=%path%;%NBP_HOME%
+set path=%path%;%ANT_HOME%;%ANT_HOME_BIN%
+set path=%path%;%ADMIN_HOME%
 
 echo Path set.
 
